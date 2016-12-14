0a0dc872f7b23398f4f42725d954c2ac7be81f96
liaosl <liaosl@boqii.com>
2016-12-14 11:37:17 +0800

    nice

--- RealCustomButton/ViewController.m
+++ RealCustomButton/ViewController.m
@@ -7,6 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "UIButton+DLButton.h"
+
+#define SCREEN_HEIGHT      [[UIScreen mainScreen] bounds].size.height
+#define SCREEN_WIDTH       [[UIScreen mainScreen] bounds].size.width
 
 @interface ViewController ()
 
@@ -17,6 +21,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // testBtn
+    UIButton* btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(0, (SCREEN_HEIGHT - 100) / 2 , SCREEN_WIDTH - 30 , 100);
+    btn.backgroundColor = [UIColor orangeColor];
+    [btn setTitle:@"hello" forState:UIControlStateNormal];
+    [btn setImage:[UIImage imageNamed:@"testImg"] forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(btnAction) forControlEvents:UIControlEventTouchUpInside];
+    
+    btn.dl_ImageRect = CGRectMake(375 - 100, 0, 100, 100);
+    btn.dl_TitleRect = CGRectMake(0, 0, 100, 100);
+    
+    [self.view addSubview:btn];
 }
 
 
@@ -25,5 +42,8 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)btnAction {
+    NSLog(@"btnAction");
+}
 
 @end
